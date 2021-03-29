ecb2369af1ff7261a4069343538c44beec5a7b32
devuri <devuriko@gmail.com>
2021-03-28 22:56:20 -0500

    feat: adds new Form\Editor

---
+++ src/Form/Editor.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace WPAdminPage\Form;
+
+/**
+ * ----------------------------------------------------------------------------
+ * @copyright 	Copyright © 2021 Uriel Wilson.
+ * @package   	FormHelper
+ * @version   	1.4.3
+ * @license   	GPL-2.0
+ * @author    	Uriel Wilson
+ * @link      	https://github.com/devuri/wp-admin-page/
+ *
+ * ----------------------------------------------------------------------------
+ */
+class Editor {
+
+	protected $content = '';
+	protected $id      = 'new_editor';
+	protected $toolbar = [];
+
+	/**
+	 * Private $instance
+	 *
+	 * @var $instance
+	 */
+	private static $instance;
+
+	/**
+	 * Singleton
+	 *
+	 * @return Editor
+	 */
+	public static function init( $id, $content ): self {
+		if ( ! isset( self::$instance ) ) {
+			self::$instance = new self( $id, $content);
+		}
+		return self::$instance;
+	}
+
+	public function __construct( string $id, $content ) {
+
+		$this->content = $content;
+		$this->id = strtolower($id);
+		$this->toolbar = [
+			'bold',
+			'italic',
+			'underline',
+			'separator',
+			'alignleft',
+			'aligncenter',
+			'alignright',
+			'separator',
+			'bullist',
+			'numlist',
+			'outdent',
+			'indent',
+			'blockquote',
+			'link',
+			'unlink',
+			'undo',
+			'redo'
+		];
+	}
+
+	/**
+  	 * The wp editor
+  	 *
+  	 * @param  string $this->content .
+  	 * @param  string $this->id .
+  	 * @param  array  $this->options .
+  	 * @return false|string
+  	 * @link https://developer.wordpress.org/reference/functions/wp_editor/
+  	 * @link https://developer.wordpress.org/reference/classes/_wp_editors/parse_settings/
+  	 */
+	public function editor() {
+	    ob_start();
+		    $args = array(
+		        'media_buttons' => false,
+		        'quicktags'     => false,
+		        'tinymce'       => array(
+		            'toolbar1' => implode(',', $this->toolbar),
+		            'toolbar2' => '',
+		            'toolbar3' => '',
+		        ),
+		    );
+		    wp_editor( $this->content, $this->id.'_textarea', $args );
+	    return ob_get_clean();
+	}
+
+	/**
+	 * Get the editor
+	 *
+	 * @param  string $this->content .
+	 * @param  string $this->id .
+	 * @param  array  $this->options .
+	 * @return string
+	 */
+	public function get() {
+
+		$name = str_replace( ' ', '_', $this->id );
+		$label = ucwords( str_replace( '_', ' ', $name ) );
+
+		return sprintf(
+			'<tr class="input">
+		    <th><label for="%1$s">%2$s</label></th>
+			    <td>%3$s</td>
+		    </tr>',
+			$name,
+			$label,
+			$this->editor()
+		);
+	}
+
+}
